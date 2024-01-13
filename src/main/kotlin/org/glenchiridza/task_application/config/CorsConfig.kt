53b8bf38c80ac7a2c1e0cc5848add75a96d69434
glenchiridza <glenchiridz@gmail.com>
2024-01-13 23:18:23 +0200

    cors configurations

---
+++ src/main/kotlin/org/glenchiridza/task_application/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package org.glenchiridza.task_application.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun getCorsConfiguration():WebMvcConfigurer{
+        return object: WebMvcConfigurer{
+            override fun addCorsMappings(registry: CorsRegistry) {
+
+                registry.addMapping("'/**")
+                    .allowedHeaders("*")
+                    .allowedMethods("*")
+                    .allowedOrigins("*")
+            }
+        }
+    }
+}
