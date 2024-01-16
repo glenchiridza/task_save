5992bd9157d4fab205c846132d1160d66f2ab226
glenchiridza <glenchiridz@gmail.com>
2024-01-16 20:50:01 +0200

    add cors

--- src/main/kotlin/org/glenchiridza/task_application/controller/TaskController.kt
+++ src/main/kotlin/org/glenchiridza/task_application/controller/TaskController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin("*")
 class TaskController(private val service: TaskService) {
 
     @GetMapping("all-tasks")
